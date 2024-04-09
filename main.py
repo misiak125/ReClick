906ea23cb16827bf5a2553afd2e97b50f05a29bf
misiak125 <kapi.msiak@gmail.com>
2024-04-09 13:46:19 +0200

    new file:   .gitignore 	modified:   main.py 	modified:   website/__init__.py 	modified:   website/__pycache__/__init__.cpython-311.pyc 	modified:   website/__pycache__/auth.cpython-311.pyc 	modified:   website/__pycache__/views.cpython-311.pyc 	modified:   website/auth.py 	modified:   website/models.py 	new file:   website/static/Image/login_img.png 	new file:   website/static/Image/main_img.png 	new file:   website/templates/base.html 	new file:   website/templates/index.html 	new file:   website/templates/login.html 	new file:   website/templates/profile.html 	new file:   website/templates/signup.html     modified:   website/views.py
        dziala? dziala
        dobrze dziala? dziala (chyba)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from website import create_app
+from website import create_app, db, models
 
 app=create_app()
 
--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,39 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME="database.db"
+
 
 def create_app():
     app=Flask(__name__)
     app.config['SECRET_KEY']='gordojestpiekny'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
 
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        
+        return User.query.get(int(user_id))
+    
     from .auth import auth
     from .views import views
 
+
     app.register_blueprint(views, url_prefx='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    with app.app_context():
+        db.create_all()
 
     return app 
