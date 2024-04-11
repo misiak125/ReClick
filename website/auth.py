3a385ddcbcc1ef8cc0a9b6aeae7ef9caa7bab41d
misiak125 <kapi.misiak@gmail.com>
2024-04-11 23:10:35 +0200

    modified:   website/auth.py 	modified:   website/models.py
        wywalilem useless includy

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from .models import User
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import check_password_hash
 from . import db
 from flask_login import login_user, login_required, current_user, logout_user, LoginManager
 from .forms import LoginForm, RegisterForm
--- website/models.py
+++ website/models.py
@@ -1,6 +1,5 @@
 from . import db
 from flask_login import UserMixin
-from sqlalchemy.sql import func
 from werkzeug.security import generate_password_hash, check_password_hash
 import datetime
 
