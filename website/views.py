50fab92cf7c66bed2c846ed4c4f420898f92b0ee
misiak125 <kapi.misiak@gmail.com>
2024-04-12 00:10:25 +0200

    new file:   website/templates/users.html 	modified:   website/views.py
        spis userow

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+from .models import User
 from . import db
 
 views=Blueprint('views', __name__)
@@ -12,3 +13,8 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
+
+@views.route('/users')
+def users():
+    users = User.query.all()
+    return render_template('users.html', users=users)
