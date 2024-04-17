c148892eb52afbec86cb96af392eeca34cc165a1
Mikorz <korzybskid@gmail.com>
2024-04-17 12:16:14 +0200

    Frontend update show password button essa

--- website/static/main.js
+++ website/static/main.js
@@ -1,27 +1,29 @@
 function togglePasswordVisibilitySignup(field1Id, field2Id) {
     var passwordField1 = document.getElementById(field1Id);
     var passwordField2 = document.getElementById(field2Id);
-    var button = event.target;
+    var keyIcon = document.getElementById("key-icon");
+
     if (passwordField1.type === "password") {
         passwordField1.type = "text";
         passwordField2.type = "text";
-        button.textContent = "Hide password";
+        keyIcon.textContent = "visibility_off";
     } else {
         passwordField1.type = "password";
         passwordField2.type = "password";
-        button.textContent = "Show password";
+        keyIcon.textContent = "visibility";
     }
 }
 
 function togglePasswordVisibilityLogin(fieldId) {
     var passwordField = document.getElementById(fieldId);
-    var button = event.target;
+    var keyIcon = document.getElementById("key-icon");
+
     if (passwordField.type === "password") {
         passwordField.type = "text";
-        button.textContent = "Hide password";
+        keyIcon.textContent = "visibility_off";
     } else {
         passwordField.type = "password";
-        button.textContent = "Show password";
+        keyIcon.textContent = "visibility";
     }
 }
 
